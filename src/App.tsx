27fe7d7f50c51a3dc9fd62f37c54e3a788af65f1
abdullahMohamed13 <https://github.com/abdullahMohamed13>
2025-10-26 22:55:57 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <main className='mt-10 flex items-center gap-3 justify-center'>
+      <img src="/logo.svg" alt="" />
+      <div>
+        Hello TEAM 👋
+      </div>
+    </main>
+  )
+}
+
+export default App
