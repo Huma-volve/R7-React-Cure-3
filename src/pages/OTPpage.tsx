e04e741ab16aa6289e0651a0436f5d8aebf47821
FawzyHany <fawzyhany1710@gmail.com>
2025-10-29 22:19:26 +0300

    Initial commit

---
+++ src/pages/OTPpage.tsx
@@ -0,0 +1,8 @@
+import { OTP } from '@/components/auth/OTP'
+
+
+export const OTPpage = () => {
+  return (
+    <OTP />
+  )
+}
--- src/routers/route.tsx
+++ src/routers/route.tsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 // Pages
 import App from '@/App.tsx'
 import DoctorDetails from '@/pages/DoctorDetails.tsx';
+import { SignupPage } from '@/pages/SignupPage';
+import { SignInPage } from '@/pages/SignInPage';
+import { ForgetPassword } from '@/pages/ForgetPassword';
+import { OTP } from '@/components/auth/OTP';
+import { OTPpage } from '@/pages/OTPpage';
 
 export const router = createBrowserRouter([
   {
@@ -19,6 +24,18 @@ export const router = createBrowserRouter([
       {
         path: 'doctors/:id/booking',
         element: <div>This is payment</div>,
+      },
+      {
+        path:"signup", element:<SignupPage/>
+      },
+      {
+        path:"signin", element:<SignInPage/>
+      },
+      {
+        path:"forget-password", element:<ForgetPassword/>
+      },
+      {
+        path:"verify", element:<OTPpage/>
       },
     ]
   }
